11014465c741f4bb7df9acc84473e8f292ee9017
Lee Davison <lee.davison@solent.ac.uk>
2021-12-25 15:45:24 +0000

    better heatmap implementation, added test data and environment.yml

--- plot.py
+++ plot.py
@@ -1,20 +1,76 @@
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
+# import numpy as np
 import pandas as pd
+
+
+import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-
-df  = pd.read_csv("locations.csv")
-# df.plot()  # plots all columns against index
-# df.plot(kind='scatter', x='x', y='y') # scatter plot
+from matplotlib import cm
+from matplotlib.colors import Normalize
+from scipy.interpolate import interpn
 
 
-# df.plot(kind='density')  # estimate density function
-# df.plot(kind='hist')  # histogram
+def density_scatter( x , y, ax = None, sort = True, bins = 20, **kwargs )   :
+    """
+    Scatter plot colored by 2d histogram
+    """
+    if ax is None :
+        fig , ax = plt.subplots()
+    data , x_e, y_e = np.histogram2d( x, y, bins = bins, density = True )
+    z = interpn( ( 0.5*(x_e[1:] + x_e[:-1]) , 0.5*(y_e[1:]+y_e[:-1]) ) , data , np.vstack([x,y]).T , method = "splinef2d", bounds_error = False)
 
-ax = df.plot.hexbin(x='x', y='y', gridsize=25, colormap='plasma')
+    #To be sure to plot all data
+    z[np.where(np.isnan(z))] = 0.0
 
-# plt.figure(figsize = (16,16))
-# plt.hist2d(x='x', y='y', bins=50, cmap=plt.cm.jet)
-plt.show()
+    # Sort the points by density, so that the densest points are plotted last
+    if sort :
+        idx = z.argsort()
+        x, y, z = x[idx], y[idx], z[idx]
+
+    ax.scatter( x, y, c=z, **kwargs )
+
+    norm = Normalize(vmin = np.min(z), vmax = np.max(z))
+    cbar = fig.colorbar(cm.ScalarMappable(norm = norm), ax=ax)
+    cbar.ax.set_ylabel('Density')
+
+    plt.show()
+
+    return ax
+
+
+if "__main__" == __name__ :
+
+    df = pd.read_csv("test_data.csv")
+    x=df.get('x')
+    y=df.get('y')
+
+
+    # x = np.random.normal(size=100000)
+    # y = x * 3 + np.random.normal(size=100000)
+    density_scatter( x, y, bins = [30,30] )
+
+
+
+
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+#
+# df = pd.read_csv("test_data.csv")
+# # df.plot()  # plots all columns against index
+# # df.plot(kind='scatter', x='x', y='y') # scatter plot
+#
+# # df.plot(kind='density')  # estimate density function
+# # df.plot(kind='hist')  # histogram
+#
+#
+#
+# ax = df.plot.hexbin(x='x', y='y', gridsize=25, colormap='plasma')
+#
+# # plt.figure(figsize = (16,16))
+# # plt.hist2d(x='x', y='y', bins=50, cmap=plt.cm.jet)
+# plt.show()
 
 
 
@@ -45,4 +101,4 @@ plt.show()
 #     plt.show()
 #
 #
-# extract_csv_gen_plot("locations.csv")
+# extract_csv_gen_plot("test_data.csv")
